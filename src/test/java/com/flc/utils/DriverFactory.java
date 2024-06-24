6fff354fea0e10e9bd56deccb30795e85773ba69
teja-ounce <teja.somala@proounce.com>
2024-06-24 23:01:41 +0530

    Changed web driver manager to manual set up of edge driver to avoid status code 500

--- src/test/java/com/flc/utils/DriverFactory.java
+++ src/test/java/com/flc/utils/DriverFactory.java
@@ -4,27 +4,20 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 	private static WebDriver driver;
-	private static String path = "C:\\Users\\Teja-OUNCE\\git\\repository\\prj231001CucumberBDD\\src\\test\\resources\\files\\msedgedriver.exe";
+	private static String path = "C:\\Users\\Teja-OUNCE\\OneDrive - proounce.com\\Documents\\GitHub\\prj231001CucumberBDD\\src\\test\\resources\\files\\msedgedriver.exe";
 
 	@SuppressWarnings("deprecation")
 	public static WebDriver getDriver() {
 		if (driver == null) {
 
-//			System.setProperty("webdriver.edge.driver",System.getenv(path));
-//			WebDriver driver = new EdgeDriver();
-			WebDriverManager.edgedriver().setup();
-			EdgeOptions options = new EdgeOptions();
-			driver = new EdgeDriver(options);
-			
-			options.addArguments("--headless"); // Enable headless mode
-			options.addArguments("--disable-gpu");
-			options.setCapability("ms:edgeOptions", options);
+			System.setProperty("webdriver.edge.driver", path);
+			driver = new EdgeDriver();
+
+//			WebDriverManager.chromedriver().setup();
+
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
 		}
