15a48634b931205436a326097fade003d630ef9e
teja-ounce <teja.somala@proounce.com>
2024-06-26 19:50:55 +0530

    53

--- src/test/java/com/flc/testRunner/TestRunner.java
+++ src/test/java/com/flc/testRunner/TestRunner.java
@@ -6,8 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class) 
-@CucumberOptions(features = "C:/Users/Teja-OUNCE/git/repository/prj231001CucumberBDD/src/test/resources/features/", glue = {
-		"com.flc.stepDefinition", "hook" }, monochrome = true,
+@CucumberOptions(features = "C:\\Users\\Teja-OUNCE\\OneDrive - proounce.com\\Documents\\GitHub\\prj231001CucumberBDD\\src\\test\\resources\\features", glue = {
+		"com.flc.stepDefinition"}, monochrome = true,
 
 		plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "html:target/htmlReport/report.html" }
 
