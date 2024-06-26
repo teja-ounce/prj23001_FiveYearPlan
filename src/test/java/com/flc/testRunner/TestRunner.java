1e816ce38d67d162dbb348cef207096819974b29
teja-ounce <teja.somala@proounce.com>
2024-06-26 21:51:53 +0530

    new changes final

--- src/test/java/com/flc/testRunner/TestRunner.java
+++ src/test/java/com/flc/testRunner/TestRunner.java
@@ -5,9 +5,16 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class) 
-@CucumberOptions(features = "C:\\Users\\Teja-OUNCE\\OneDrive - proounce.com\\Documents\\GitHub\\prj231001CucumberBDD\\src\\test\\resources\\features", glue = {
-		"com.flc.stepDefinition"}, monochrome = true,
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {
+		"C:\\Users\\Teja-OUNCE\\OneDrive - proounce.com\\Documents\\GitHub\\prj231001CucumberBDD\\src\\test\\resources\\features\\Login.feature",
+		"C:\\Users\\Teja-OUNCE\\OneDrive - proounce.com\\Documents\\GitHub\\prj231001CucumberBDD\\src\\test\\resources\\features\\DataSourcesfeature",
+		"C:\\Users\\Teja-OUNCE\\OneDrive - proounce.com\\Documents\\GitHub\\prj231001CucumberBDD\\src\\test\\resources\\features\\Dashboard.feature",
+		"C:\\Users\\Teja-OUNCE\\OneDrive - proounce.com\\Documents\\GitHub\\prj231001CucumberBDD\\src\\test\\resources\\features\\IBAPSExtract.feature"
+
+},
+
+		glue = { "com.flc.stepDefinition" }, monochrome = true,
 
 		plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "html:target/htmlReport/report.html" }
 
