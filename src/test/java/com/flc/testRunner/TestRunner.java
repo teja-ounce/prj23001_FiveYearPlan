d432575ac86877df3bbeda21578011351523372d
teja-ounce <teja.somala@proounce.com>
2024-06-28 22:19:51 +0530

    s

--- src/test/java/com/flc/testRunner/TestRunner.java
+++ src/test/java/com/flc/testRunner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\proou\\eclipse-workspace\\prj23001_cucumber-master\\prj23001_cucumber-master\\src\\test\\resources\\features",
+@CucumberOptions(features = "C:\\Proounce Project\\prj23001_FiveYearPlan\\src\\test\\resources\\features",
 
 		glue = { "com.flc.stepDefinition" }, monochrome = true,
 
